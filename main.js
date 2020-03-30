a5d23c38879e140cd15b72c6b5b748dba6ea66e2
JMHenri <jakehenri@gmail.com>
2020-03-29 21:10:41 -0400

    express server for dist, live server for dev.

---
+++ main.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const Datastore = require('nedb');
+const db = new Datastore({
+  filename: './datastore',
+  autoload: true
+});
+const fs = require('fs');
+
+try {
+  if (fs.existsSync('./datastore')) {} else {
+    populateDataStore();
+  }
+} catch (err) {
+  console.error(err)
+}
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.listen(port, () => {
+  console.log("App listening on port : ", port);
+});
+
+
+
+function populateDataStore() {
+  const doc = JSON.parse(fs.readFileSync('./DATA.json', 'UTF-8'));
+  db.insert(doc);
+}
